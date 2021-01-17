72587f5cd79c4696a6348fbd9466d710cf51dd15
LM Bibera <lyndon.bibera@headhuntr.io>
2021-01-17 19:18:35 +0800

    fix that issue with MN where we expect the reloading of context - but failed, so we ran the test into separate classes

--- src/test/groovy/net/miiingle/user/api/presentation/RegistrationControllerWithErrorSpec.groovy
+++ src/test/groovy/net/miiingle/user/api/presentation/RegistrationControllerWithErrorSpec.groovy
@@ -2,35 +2,34 @@ package net.miiingle.user.api.presentation
 
 import io.micronaut.context.annotation.Property
 import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import net.miiingle.user.api.business.data.Registration
 import spock.lang.Specification
 
 import javax.inject.Inject
 
-
 @MicronautTest
-@Property(name = "client.email", value = "logging")
-class RegistrationControllerSpec extends Specification {
+@Property(name = "client.email", value = "error")
+class RegistrationControllerWithErrorSpec extends Specification {
 
     @Inject
     @Client("/")
     HttpClient client
 
-    def "should register"() {
+    def "should throw a generic error when an exception is encountered"() {
 
         given:
         HttpRequest request = HttpRequest.POST("/register", Registration.builder().build())
 
         when:
-        HttpResponse response = client.toBlocking().exchange(request)
+        client.toBlocking().exchange(request)
 
         then:
-        response.status == HttpStatus.OK
+        def response = thrown(HttpClientResponseException)
+        response.status == HttpStatus.INTERNAL_SERVER_ERROR
     }
-
 }
